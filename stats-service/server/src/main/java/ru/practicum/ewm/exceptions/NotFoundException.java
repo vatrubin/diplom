bebeeecbf05fe5552430b068941f8c06d9f8e1bc
anastasiaelis <anastasia_elis@mail.ru>
2024-08-22 21:28:30 +0500

    statS-service

--- stats-service/server/src/main/java/ru/practicum/ewm/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.ewm.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}
